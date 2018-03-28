ca6f2d1b5aa8dd305173a3f63f842ffefc1fd6d7
sesevgen <sesevgen@gmail.com>
2018-03-27 23:19:32 -0500

    Forward prop of derivative working in prototype

--- python_prototype.py
+++ python_prototype.py
@@ -11,7 +11,7 @@ hidden_layer2 = 2
 output_dim = 1
 
 #Training data size
-dataset = 1
+dataset = 50
 
 # sigmoid function
 def nonlin(x,deriv=False):
@@ -21,17 +21,17 @@ def nonlin(x,deriv=False):
 	return 1/(1+np.exp(-x))
     
 # input dataset
-X = np.linspace(0.2,1,dataset)
+X = np.linspace(0.0,1,dataset)
     
 # output dataset            
 y = np.power(X,2) + 0.0*np.random.random(dataset)
 
-# derivative dataset
-z = 2*X
-
 # switch to column format
 X = np.reshape(X,(dataset,1))
 y = np.reshape(y,(dataset,1))
+
+# derivative dataset
+z = 2*X
 
 # seed random numbers to make calculation
 # deterministic (just a good practice)
@@ -51,32 +51,16 @@ for iter in xrange(20000):
 	l2 = nonlin(np.dot(l1,syn1))
 	l3 = nonlin(np.dot(l2,syn2))
 
-	print l0
-	print l1
-	print l2
-	print l3
-
-	print " "
-	print " "
-
 	# forward propogation of derivative
 	l0d = np.ones((dataset,input_dim))
-	l1d = np.dot(l0d,syn0)
-	print l1d
-	print nonlin(l1,True)
-	print syn0	
-	print " " 
-	print l1d*nonlin(l1,True)	
+	l1d = np.dot(l0d,syn0)*nonlin(l1,True)
+	l2d = np.dot(l1d,syn1)*nonlin(l2,True)
+	l3d = np.dot(l2d,syn2)*nonlin(l3,True)
 
-	#l1d = nonlin(l1,True)np.dot(l0d,syn0)
-	#print l1d
-	#l2d = nonlin(l2,True)*l1d
-	#l3d = nonlin(l3,True)*l2d
-
-	exit()
-
-	# backward propogation
+	# define error	
 	l3_error = y - l3
+
+	# backward propogation	
 	l3_delta = l3_error * nonlin(l3,deriv=True)
 
 	l2_error = l3_delta.dot(syn2.T)
@@ -94,18 +78,13 @@ for iter in xrange(20000):
 	if (iter % 1000 == 0):
 		print "Error: " +str(np.mean(np.abs(l3_error)))
 
-print "Output After Training:"
 plt.figure()
 plt.plot(X,y,X,l3)
 plt.show()
 
 plt.figure()
-plt.plot(X,np.gradient(y[:,0],X[1]-X[0]),X,l3d)
+plt.plot(X,np.gradient(l3[:,0],X[1]-X[0]),X,l3d)
 plt.show()
-
-print np.gradient(y[:,0],X[1]-X[0])
-print l3d
-exit()
 
 # now try to teach from derivative
 
@@ -115,7 +94,7 @@ syn1 = 2*np.random.random((hidden_layer1,hidden_layer2)) - 1
 syn2 = 2*np.random.random((hidden_layer2,output_dim)) - 1
 
 
-for iter in xrange(20000):
+for iter in xrange(10000):
 
 	# forward propagation
 	l0 = X
@@ -123,16 +102,27 @@ for iter in xrange(20000):
 	l2 = nonlin(np.dot(l1,syn1))
 	l3 = nonlin(np.dot(l2,syn2))
 
-	# backward propogation
-	l3_der_error = z - l3
+	# forward propogation of derivative
+	l0d = np.ones((dataset,input_dim))
+	l1d = np.dot(l0d,syn0)*nonlin(l1,True)
+	l2d = np.dot(l1d,syn1)*nonlin(l2,True)
+	l3d = np.dot(l2d,syn2)*nonlin(l3,True)
 
-	l3_delta = l3_error * nonlin(l3,deriv=True)
+	# define error
+	l3_error = (y-l3)	
+
+	# define error on the derivative
+	l3_der_error = (z - l3d)	
+
+	# backward propogation
+	# second derivative of nonlinearity is simply f*f'
+	l3_delta = l3_der_error*(nonlin(l3,True)*l3d+nonlin(l3,True)*l3*l3d
 
 	l2_error = l3_delta.dot(syn2.T)
-	l2_delta = l2_error * nonlin(l2,deriv=True)
+	l2_delta = l2_error*(nonlin(l2,True)*l2)*l2d
 
 	l1_error = l2_delta.dot(syn1.T)	
-	l1_delta = l1_error * nonlin(l1,deriv=True)
+	l1_delta = l1_error*(nonlin(l1,True)*l1)*l1d
 
 	# update weights
 	syn0 += np.dot(l0.T,l1_delta)
@@ -141,13 +131,13 @@ for iter in xrange(20000):
 
 	# print error
 	if (iter % 1000 == 0):
-		print "Error: " +str(np.mean(np.abs(l3_error)))
+		print "Error: " +str(np.mean(np.abs(l3_der_error)))
 
-print "Output After Training:"
 plt.figure()
 plt.plot(X,y,X,l3)
 plt.show()
 
-
-
+plt.figure()
+plt.plot(X,np.gradient(l3[:,0],X[1]-X[0]),X,l3d,X,z)
+plt.show()
 
